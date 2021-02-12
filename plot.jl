6ce3799e3a2df3d991f8f20d9ae2c3670f52c00a
Alec Arreche <alec.arreche@me.com>
2021-02-12 13:23:16 -0500

    plotting functionality

--- plot.jl
+++ plot.jl
@@ -1,16 +1,35 @@
-using SQLite
 using DataFrames
+using Gadfly
+using SQLite
 
-db_file = ARGS[1] # "names.db"
-name = ARGS[2]
-sex = ARGS[3]
+# params
+db_file = ARGS[1] 
+name = uppercase(ARGS[2])
+sex = uppercase(ARGS[3])
 
+# query 
 db = SQLite.DB(db_file)
 qry = DBInterface.prepare(db,
         "SELECT year, num
         FROM names
-        WHERE name = ? AND sex = ?
-        ORDER BY year")
+        WHERE UPPER(name) = ? AND UPPER(sex) = ?
+        ORDER BY year"
+)
 
 result = DBInterface.execute(qry, [name, sex]) |> DataFrame
-print(result)
+
+# plot
+sex_color = Dict("M"=>"light blue", "F"=>"pink")
+
+fig = plot(
+        result,
+        x=:year,
+        y=:num,
+        Geom.line,
+        Theme(default_color=color(sex_color[sex])),
+        Guide.xlabel("Year"),
+        Guide.ylabel("Babies Born"),
+        Guide.title("Popularity of Baby Name $name")
+)
+
+display(fig)
