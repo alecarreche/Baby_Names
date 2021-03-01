5775fa6aaeb07eb0f1b407635e2629628f45e142
Alec Arreche <alec.arreche@me.com>
2021-02-28 21:25:06 -0500

    task 5, fixed dictionary construction without find()

--- hw3/similar_names.jl
+++ hw3/similar_names.jl
@@ -21,16 +21,40 @@ Ng = size(unique_g, 1)
 Ny = size(unique_y, 1)
 
 # Task 3 - dictionaries 
-boy_idx = Dict(key => findall(x -> x == key, unique_b) for key in unique_b)
-boy_name = Dict(value => key for (key, value) in boy_idx)
 
-girl_idx = Dict(key => findall(x -> x == key, unique_g) for key in unique_g)
-girl_name = Dict(value => key for (key, value) in girl_idx)
+# dictionaries without using find()
+boy_name = Dict(enumerate(unique_b))
+boy_idx = Dict(value => key for (key, value) in boy_name)
 
-year_idx = Dict(key => findall(x -> x == key, unique_y) for key in unique_y)
-year_name = Dict(value => key for (key, value) in year_idx)
+girl_name = Dict(enumerate(unique_g))
+girl_idx = Dict(value => key for (key, value) in girl_name)
 
-# Task 4 - Frequency Matrices
+year_name = Dict(enumerate(unique_y))
+year_idx = Dict(value => key for (key, value) in year_name)
+
+# Task 4-5 - Frequency Matrices
 Fb = zeros((Nb, Ny))
 Fg = zeros((Ng, Ny))
 
+boy_groups = groupby(df[df.sex .== "M", :], [:"year", :"name"])
+girl_groups = groupby(df[df.sex .=="F", :], [:"year", "name"])
+
+for (i, group) in enumerate(boy_groups)
+    cur_name = keys(boy_groups)[i][:"name"]
+    cur_year = keys(boy_groups)[i][:"year"]
+
+    cur_name_idx = boy_idx[cur_name]
+    cur_year_idx = year_idx[cur_year]
+
+    Fb[cur_name_idx, cur_year_idx] = group[1, :"num"]
+end
+
+for (i, group) in enumerate(girl_groups)
+    cur_name = keys(girl_groups)[i][:"name"]
+    cur_year = keys(girl_groups)[i][:"year"]
+
+    cur_name_idx = girl_idx[cur_name]
+    cur_year_idx = year_idx[cur_year]
+
+    Fg[cur_name_idx, cur_year_idx] = group[1, :"num"]
+end
