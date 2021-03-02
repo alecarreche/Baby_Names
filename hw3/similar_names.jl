74f5cb22cbccae5d8efabbc3fbe6100340775f3c
Alec Arreche <alec.arreche@me.com>
2021-03-01 19:28:20 -0500

    finished main tasks

--- hw3/similar_names.jl
+++ hw3/similar_names.jl
@@ -1,4 +1,7 @@
 using DataFrames
+using Distributed
+using LinearAlgebra
+using SharedArrays
 using SQLite
 
 # Task 1 - load data 
@@ -58,3 +61,64 @@ for (i, group) in enumerate(girl_groups)
 
     Fg[cur_name_idx, cur_year_idx] = group[1, :"num"]
 end
+
+# Task 6 - Yearly totals 
+
+yearly_sum = reshape(sum(eachrow(Fb)) + sum(eachrow(Fg)), (1, Ny))
+
+# Task 7 - probabilities 
+
+Pb = Fb ./ yearly_sum
+Pg = Fg ./ yearly_sum
+
+# Task 8 - L2 Norm accross years
+
+Qb = deepcopy(Pb)
+Qg = deepcopy(Pg)
+
+for i in 1:Nb
+    Qb[i,:] = normalize(Qb[i,:])
+end
+
+for i in 1:Ng
+    Qg[i,:] = normalize(Qg[i,:])
+end
+
+# Task 9 and 10 - Find cosine distances 
+b_size = div(Nb, 10)
+g_size = div(Ng, 10)
+
+# create sections
+
+# first 9
+Qb_sections = [Qb[b_size*(i-1)+1:b_size*i, :] for i in 1:9]
+Qg_sections = [Qg[g_size*(i-1)+1:g_size*i, :] for i in 1:9]
+
+# last section (includes remainder)
+push!(Qb_sections, Qb[b_size*9+1:Nb, :])
+push!(Qg_sections, Qg[b_size*9+1:Ng, :])
+
+glob_max = 0
+max_coord = CartesianIndex(0,0)
+# multiply
+@time begin
+    for i in 1:10
+        for j in 1:10
+            result = Array{Float64}(undef, size(Qb_sections[i])[1], size(Qg_sections[j])[1])
+            mul!(result, Qb_sections[i], transpose(Qg_sections[j]))
+            loc_max = maximum(result)
+            
+            if loc_max > glob_max
+                m = argmax(result)
+                global max_coord = CartesianIndex(m[1] + (i-1)*b_size, m[2] + (i-1)*g_size)
+                global glob_max = loc_max
+            end
+        end
+    end
+end
+
+max_boy = boy_name[max_coord[1]]
+max_girl = girl_name[max_coord[2]]
+println("Max Cosine Distance: ")
+println("Boy: $max_boy")
+println("Girl: $max_girl")
